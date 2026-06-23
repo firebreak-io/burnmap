82546014d61af8fd8aae737f1b08013932d4c63e
Chris Brady <chris@firebreak.io>
2026-06-22 22:17:53 -0600

    feat(shoot): captionPng composites a caption strip onto a PNG

--- packages/shoot/src/index.ts
+++ packages/shoot/src/index.ts
@@ -2,3 +2,4 @@ export { buildShotHtml } from './html.js';
 export { resolveWebDist, writeShotHtml, cleanupShotHtml, SHOT_HTML_NAME } from './web-dist.js';
 export { capture, type CaptureOptions } from './capture.js';
 export { rasterizeSvg } from './svg-shot.js';
+export { captionPng } from './caption.js';
